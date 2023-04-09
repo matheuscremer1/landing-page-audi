940235ca0d78f96d6c73a67c68f9e7649ecd04a9
Matheus Cremer <126881869+matheuscremer1@users.noreply.github.com>
2023-04-09 18:27:43 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function load(){
+    const logo = document.querySelector("div#nome")
+    if(logo.style.opacity = '0%'){
+        logo.style.opacity = '100%'
+    }else{
+        logo.style.opacity = '0%'
+    }
+ 
+}
